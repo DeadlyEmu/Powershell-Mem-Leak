43944f0a67a3381a72ceb6b4fc93859fa1abc098
Ryan <Ryan@fluxvision.co.uk>
2024-01-18 19:56:45 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Management.Automation.Language;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine($"Loop {i++}");
+                //Currently used memory
+                Console.WriteLine($"Memory: {GC.GetTotalMemory(false)}");
+                Task.Delay(3000).Wait();
+                await powershell.Trigger();
+                
+
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApp1/powershell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class powershell
+    {
+
+        public static async Task Trigger()
+        {
+                PowerShell ps = PowerShell.Create();
+                ScriptBlock scriptBlock = ScriptBlock.Create($@"Import-Module RemoteDesktop
+                Get-RDUserSession");
+                ps.AddScript("& $args[0]").AddArgument(scriptBlock);
+
+                StringBuilder sb = new StringBuilder();
+
+                var pipelineObjects = await ps.InvokeAsync().ConfigureAwait(false);
+                foreach (var item in pipelineObjects)
+                {
+                    sb.AppendLine(item.ToString());
+                }
+                pipelineObjects.Dispose();
+                Console.Write(sb.ToString());
+                ps.Dispose();
+        }
+    }
+}
